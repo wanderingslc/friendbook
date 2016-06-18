3e1d09a7559864141c9981adc00654dd69e0c006
wanderingslc <stan.martineau@gmail.com>
2016-06-18 14:54:45 -0600

    Style updates and add validations

--- src/clj/friendbook/routes/home.clj
+++ src/clj/friendbook/routes/home.clj
@@ -1,17 +1,39 @@
 (ns friendbook.routes.home
-  (:require [friendbook.layout :as layout]
+  (:require
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [friendbook.layout :as layout]
             [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [friendbook.db.core :as db]))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html" {:messages (db/get-messages)}))
 
 (defn about-page []
   (layout/render "about.html"))
 
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+        (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (POST "/message" request (save-message! request))
+  (GET "/about" [] (about-page))
+           )
 
